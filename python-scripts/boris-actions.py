0579529c881c50909f3016a177700693e6a27de0
April Novak <novak@berkeley.edu>
2017-01-18 16:03:23 -0500

    added create_unique_pins to boris-actions

--- python-scripts/boris-actions.py
+++ python-scripts/boris-actions.py
@@ -4,4 +4,8 @@ import sys
 filename = sys.argv[1]
 base_filename = filename[:-4]
 
+# convert the geometry to XML form
 os.system("~/projects/VERA/VERAInExt/verain/scripts/react2xml.pl {0} {1}.xml".format(filename, base_filename))
+
+# add create_unique_pins parameter
+os.system("python ~/projects/nkq_analysis/python-scripts/insert-create-unique-pins.py {0}.xml".format(base_filename))
