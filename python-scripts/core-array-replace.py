184aec8431f24b0f255d012eff2af0711df83d01
April Novak <novak@berkeley.edu>
2017-01-18 16:39:03 -0500

    added find-and-replace.

--- python-scripts/core-array-replace.py
+++ python-scripts/core-array-replace.py
@@ -6,4 +6,6 @@ base_filename = filename[:-7]
 
 with open(filename, 'r') as fi:
     with open(base_filename + "-replaced.gg.xml", 'w') as fo:
-        print(base_filename + "-replaced.gg.xml")
+        print("Performing find-and-replace of rtk_fuel by core_array")
+	for line in fi:
+            fo.write(line.replace("rtk_fuel", "core-array"))
