d8d1a9ab12f57494f39f52f224e8991abd1ae32e
April Novak <novak@berkeley.edu>
2017-01-18 17:03:25 -0500

    added script to automatically find and replace my path for notebook mounting.

--- python-scripts/find-replace-path.py
+++ python-scripts/find-replace-path.py
@@ -2,13 +2,15 @@ import sys
 
 filename = sys.argv[1]
 
-base_filename = filename[:-7]
+base_filename = filename[:-16]
+
+print(base_filename)
 
 find = sys.argv[2]
 replace = sys.argv[3]
 
 with open(filename, 'r') as fi:
-    with open(base_filename + "-replaced.gg.xml", 'w') as fo:
+    with open(base_filename + "-notebook.gg.xml", 'w') as fo:
         print("Performing find-and-replace of {0} by {1}".format(find, replace))
-	for line in fi:
+        for line in fi:
             fo.write(line.replace(find, replace))
