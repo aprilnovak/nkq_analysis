20320044b7250a89befd07415ef3b39efb6cabd5
April Novak <novak@berkeley.edu>
2017-01-18 16:07:00 -0500

    added vera_to_shift run.

--- python-scripts/boris-actions.py
+++ python-scripts/boris-actions.py
@@ -9,3 +9,6 @@ os.system("~/projects/VERA/VERAInExt/verain/scripts/react2xml.pl {0} {1}.xml".fo
 
 # add create_unique_pins parameter
 os.system("python ~/projects/nkq_analysis/python-scripts/insert-create-unique-pins.py {0}.xml".format(base_filename))
+
+# run the vera_to_shift executable
+os.system("mpirun -np 12 ~/builds/PSSDriversExt/VeraShift/src/vera_to_shift --case=\"{0}\"".format(base_filename))
