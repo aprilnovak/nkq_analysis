7b2a950dbd098fe73b04af94ae0404f45997d2d4
April Novak <novak@berkeley.edu>
2017-01-18 16:44:25 -0500

    rename file.

--- python-scripts/core-array-replace.py
+++ python-scripts/core-array-replace.py
@@ -4,8 +4,11 @@ filename = sys.argv[1]
 
 base_filename = filename[:-7]
 
+find = "rtk_fuel"
+replace = "core-array"
+
 with open(filename, 'r') as fi:
     with open(base_filename + "-replaced.gg.xml", 'w') as fo:
-        print("Performing find-and-replace of rtk_fuel by core_array")
+        print("Performing find-and-replace of {0} by {1}".format(find, replace))
 	for line in fi:
-            fo.write(line.replace("rtk_fuel", "core-array"))
+            fo.write(line.replace(find, replace))
