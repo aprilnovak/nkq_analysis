ed0a0f1973152b04259e7272337deff943a1d0a5
April Novak <novak@berkeley.edu>
2017-01-19 08:37:23 -0500

    Updated orthanc-actions.py

--- python-scripts/orthanc-actions.py
+++ python-scripts/orthanc-actions.py
@@ -4,10 +4,15 @@ import sys
 filename = sys.argv[1]
 base_filename = filename[:-4]
 
+print(base_filename)
+
+print("Adding new compositions to {0}.shift_compositions.h5".format(base_filename))
 os.system("python ~/projects/nkq_analysis/python-scripts/add-comps.py {0}.shift_compositions.h5".format(base_filename))
 
+print("Generating XML version of GG file")
 os.system("~/install/bin/gg2xml {0}.gg.omn".format(base_filename))
 
+print("Renaming generated XML file")
 os.system("mv {0}.gg.gg.xml {0}.gg.xml".format(base_filename))
 
 os.system("python ~/projects/nkq_analysis/python-scripts/find-replace.py {0}.gg.xml rtk_fuel core-array".format(base_filename))
