c02975c98eaf877ad3def0a0f9a44f8706e0efd1
April Novak <novak@berkeley.edu>
2017-02-12 22:34:34 -0500

    uncommented duplicate line in orthanc-actions.py

--- python-scripts/orthanc-actions.py
+++ python-scripts/orthanc-actions.py
@@ -21,7 +21,7 @@ os.system("mv {0}.gg.gg.xml {0}.gg.xml".format(base_filename))
 
 os.system("omnibus-run {0}.omn".format(base_filename))
 
-os.system("python ~/projects/nkq_analysis/python-scripts/find-replace.py {0}.gg.xml rtk_fuel core-array".format(base_filename))
+#os.system("python ~/projects/nkq_analysis/python-scripts/find-replace.py {0}.gg.xml rtk_fuel core-array".format(base_filename))
 
 os.system("python ~/projects/nkq_analysis/python-scripts/find-replace-path.py {0}-replaced.gg.xml /home/nkq/ /Users/aprilnovak/Mounts/orthanc/".format(base_filename))
 
