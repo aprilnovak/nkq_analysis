48d2b528907e4c6079d3713c2218cb4bfe63e4bb
April Novak <novak@berkeley.edu>
2017-03-20 19:42:57 -0400

    modified the extract-fission-source.py script to automatically convert all of the fission sources for a multi-state run into separate files for input into Omnibus files.

--- python-scripts/extract-fission-source.py
+++ python-scripts/extract-fission-source.py
@@ -12,42 +12,40 @@ class ExtractFissionSource:
 
         self.filename = filename
 
-    def extract_statepoint(self, statepoint):
+    def extract_statepoint(self):
         """ Method to extract the fission source from an HDF5 file.
 
-            This function takes a statepoint (referring to one of the possibly
-            multiple [STATE] blocks in a VERA input). The statepoint is an
-            integer that refers to the position in time of the statepoint.
-            For instance - the first statepoint is 0, the second 1, etc."""
+	    This function extracts all unique statepoints in the *.shift.h5 
+	    input file and writes one output file for each state that only
+    	    consists of the fission_source group. """
 
-        # suffix of the filename that is appended by the vera_to_shift executable
-        suffix_in = ".shift.h5"
-
-        # output filename
-        filename_out = self.filename[:-len(suffix_in)] + ".src-extracted" + self.filename[-len(suffix_in):]
-
+	# expected suffix for input to this file
+	suffix_in = ".shift.h5"
+	
         # open the HDF5 file for reading
         with h5py.File(self.filename, "r") as fi:
+	    # only extract the fission source data sets. This assumes that the number of groups in the
+	    # fission source file is 3 + the number of states and that the first state is the second entry
+	    # (CORE is the first entry)
+	    input_groups = fi.values()
 
-            # open an HDF5 file for writing
-            with h5py.File(filename_out, "w") as fo:
-                # extract the entire statepoint group
-                #input_keys = fi.keys()
-                #fi.copy('/{0}'.format(input_keys[statepoint]), fo)
-
-                # only extract the fission source data sets
-                input_groups = fi.values()
-                fission_source = input_groups[statepoint]["fission_source"]
-
-                for i in fission_source.keys():
-                    fo.create_dataset(str(i), data=fission_source[str(i)])
-
-try:
-    print("Converting fission source for {0}".format(sys.argv[1]))
-    #print(os.getcwd() + '/' + str(sys.argv[1]))
-    instance = ExtractFissionSource(os.getcwd() + '/' + str(sys.argv[1]))
-    instance.extract_statepoint(0)
-except:
-    print("""Error: Syntax for this script is:\n
-    python extract-fission-source.py <filename>.shift.h5.\n\n
-    This script should be run from the directory where the original HDF5 file is located.""")
+	    for s in range(len(input_groups) - 3):
+		    # output filename
+		    filename_out = self.filename[:-len(suffix_in)] + ".src-extracted_state_" + str(s + 1) + ".shift.h5"
+		    
+		    # open an HDF5 file for writing
+		    with h5py.File(filename_out, "w") as fo:
+			try:
+				# loop through all of the entries to determine which group corresponds to source
+				fission_source = input_groups[s + 1]["fission_source"]
+				
+				for i in fission_source.keys():
+					fo.create_dataset(str(i), data=fission_source[str(i)])
+			except:
+			    print("""Error: Syntax for this script is:\n
+			    python extract-fission-source.py <filename>.shift.h5.\n\n
+			    This script should be run from the directory where the original HDF5 file is located. This script has failed for some reason, check that the file exists and that it is in the correct format.""")
+    
+print("Converting fission source for {0}".format(sys.argv[1]))
+instance = ExtractFissionSource(os.getcwd() + '/' + str(sys.argv[1]))
+instance.extract_statepoint()
